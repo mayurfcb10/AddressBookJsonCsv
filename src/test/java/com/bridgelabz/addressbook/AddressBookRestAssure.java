77569ae8d4e1894c72c0748555ca2967e925207f
Mayur <mayurzope555@gmail.com>
2020-11-07 11:35:46 +0530

    [Mayur]Added: Test Case to read and update the contact data

--- src/test/java/com/bridgelabz/addressbook/AddressBookRestAssure.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookRestAssure.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbook;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.Assert;
@@ -10,10 +11,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.bridgelabz.addressbook.AddressBookService.IOService;
+
 import com.google.gson.Gson;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class AddressBookRestAssure {
 static AddressBookService addressBookService;
@@ -37,7 +40,7 @@ static AddressBookService addressBookService;
 	/* get Contact List from the AddressBook Json */
 	public ContactDetails[] getContactList(){
         Response response = RestAssured.get("/contacts");
-        System.out.println("Employee Payroll entires in JSON Server: \n"+response.asString());
+        System.out.println("Address Book entires in JSON Server: \n"+response.asString());
         ContactDetails[] arrayOfContacts = new Gson().fromJson(response.asString(),ContactDetails[].class);
         return arrayOfContacts;
     }
@@ -49,5 +52,52 @@ static AddressBookService addressBookService;
 	        long entries = addressBookService.countEntries(IOService.REST_IO);
 	        Assert.assertEquals(2,entries);
 	    }
-
+	 
+	 
+	 private Response addContactToJsonServer(ContactDetails contactData) {
+			String employeeJson = new Gson().toJson(contactData);
+			RequestSpecification request = RestAssured.given();
+			request.header("Content-Type", "application/json");
+			request.body(employeeJson);
+			return request.post("/contacts");
+		}
+		
+		/* Added Contact in Employee Payroll Json */
+		@Test
+		public void givenContactListWhenAdded_shouldMatch201andCount() throws AddressBookException {
+			ContactDetails[] arrayOfEmps = getContactList();
+			addressBookService = new AddressBookService(Arrays.asList(arrayOfEmps));
+			
+			ContactDetails contactData = null;
+			contactData = new ContactDetails("Sharan", "Alawa", "Indus Town", "Rajannagar", "Maharastra",
+					"sharan.eee18@gmail.com", "9354895489", "400037", 3, 105, "friend", LocalDate.of(2018, 8, 28));
+			
+			Response response = addContactToJsonServer(contactData);
+			int statusCode = response.getStatusCode();
+			Assert.assertEquals(201, statusCode);
+			
+			contactData = new Gson().fromJson(response.asString(), ContactDetails.class);
+			addressBookService.addContactDetails(contactData, IOService.REST_IO);
+		 long entries = addressBookService.countEntries(AddressBookService.IOService.REST_IO);
+		 Assert.assertEquals(3,entries);
+			
+		}
+		
+		/* Updated data of contact in Employee Payroll Json */
+		@Test
+		public void givenFirstNameandLastName_whenUpdated_ShouldMatch200Response() throws AddressBookException {
+			ContactDetails[] arrayOfEmps = getContactList();
+			addressBookService = new AddressBookService(Arrays.asList(arrayOfEmps));
+			
+			addressBookService.updateContactInformation("Sharan","Ahluwalia",IOService.REST_IO);
+			ContactDetails contactData = addressBookService.getContactData("Sharan");
+			
+			String employeeJson = new Gson().toJson(contactData);
+			RequestSpecification request = RestAssured.given();
+			request.header("Content-Type", "application/json");
+			request.body(employeeJson);
+			Response response = request.put("/contacts/"+contactData.userId);
+			int statusCode = response.getStatusCode();
+	    	Assert.assertEquals(200, statusCode);
+		}
 }
